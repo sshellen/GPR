e7e493c2fd99764f576edd5ddd3a649592028acf
Steinar <sshellen@users.noreply.github.com>
2022-03-18 23:04:25 -0700

    Adding pages to the src folder

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,62 @@
+import React from "react";
+import TopPage from "../../components/TopPage/TopPage";
+import Footer from "../../components/Footer/Footer";
+import "./css/index.css";
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <TopPage />
+        <main>
+          <h1>Contact</h1>
+          <div className="contact">
+            <h2>CONTACT FORM</h2>
+            <form>
+              <div className="formRow">
+                <label htmlFor="name">
+                  Name<sup>*</sup>{" "}
+                </label>
+                <input id="name" type="text" />
+              </div>
+
+              <div className="formRow">
+                <label htmlFor="email">
+                  Email<sup>*</sup>{" "}
+                </label>
+                <input id="email" type="text" />
+              </div>
+
+              <div className="formRow">
+                <label htmlFor="subject">Subject</label>
+                <input id="subject" type="text" />
+              </div>
+
+              <div className="formRow">
+                <label htmlFor="message">
+                  Message<sup>*</sup>{" "}
+                </label>
+                <textarea id="message" type="text"></textarea>
+              </div>
+
+              <div className="requiredFields">
+                <sup>*</sup>Required fields
+              </div>
+              <div className="buttonBar">
+                <button className="blueButton">SEND MESSAGE</button>
+              </div>
+            </form>
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import qs from "qs";
+import TopPage from "../../components/TopPage/TopPage";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Footer from "../../components/Footer/Footer";
+import HomeEventsAndNews from "../../components/HomeEventsAndNews/HomeEventsAndNews";
+import ArtistTiles from "../../components/ArtistTiles/ArtistTiles";
+import "./css/index.css";
+import bannerImg from "/img/homeBanner.png";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { artists: [] };
+  }
+
+  fetchData = async () => {
+    const query = qs.stringify({
+      filters: {
+        featured: {
+          $eq: true,
+        },
+      },
+    });
+    const request = `${process.env.API_URL}/api/artists?${query}&populate=*`;
+
+    let data = await fetch(request, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return data;
+  };
+
+  componentDidMount() {
+    this.fetchData()
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ artists: data.data });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <TopPage />
+        <div className="homeBanner">
+          <div className="bannerContent">
+            <div className="slogan">Create a Better World Through Music</div>
+            <p className="tagline">
+              Global Peace Rhythms features artists who are making positive
+              changes on earth.
+            </p>
+            <button
+              className="blueButton"
+              onClick={() => this.props.history.push("./about")}
+            >
+              LEARN MORE
+            </button>
+          </div>
+          <img src={bannerImg} />
+        </div>
+        <main>
+          <h1>Featured Artists</h1>
+          <div className="featuredArtists">
+            {this.state.artists && this.state.artists.length > 0 && (
+              <ArtistTiles artists={this.state.artists} />
+            )}
+
+            <Sidebar />
+          </div>
+          <HomeEventsAndNews />
+        </main>
+
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
